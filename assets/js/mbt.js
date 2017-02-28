11636a8ee4552f2b30f92e89178867535687fc17
qwewqa <krcgame@gmail.com>
2017-02-28 10:37:25 -0500

    Update mbt.js

--- assets/js/mbt.js
+++ assets/js/mbt.js
@@ -2,7 +2,7 @@
 $('.well').addClass('animated zoomIn');
 $('.alert').addClass('animated zoomIn');
 */
-window.onload = "onLoad();"
+window.onload = onLoad();
 function onLoad() {
   var aud = document.getElementById("backgroundMusic");
   aud.volume = 0.4;
